78d3fdd53657c8e0eeba03c7c4017a3fa148e657
Anika Morshed <anikamorshed4434@gmail.com>
2024-07-13 01:22:56 +0600

    done hospitalpage

--- frontend/src/components/general/Header.js
+++ frontend/src/components/general/Header.js
@@ -2,13 +2,18 @@ import { react, Fragment } from 'react'
 import './general.css'
 import './header.css'
 import img1 from './logo.svg'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+    const goHome = () => {
+        window.location = '/home'
+    }
     return <Fragment>
         <header>
             <div class="header-icons">
                 <div class="logo-section">
-                    <img src={img1} alt="logo" className="logo-icon" />
+                    {/* <Link to="/home" className="link-style"></Link> */}
+                    <img src={img1} alt="logo" className="logo-icon" onClick={goHome} />
                     <div class="description">
                         Personal Healthcare Assistant
                     </div>
