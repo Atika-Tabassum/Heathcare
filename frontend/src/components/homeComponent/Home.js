96ff3c1209cab936131b9432ffddc9da41c9ed76
Anika Morshed <anikamorshed4434@gmail.com>
2024-07-12 15:52:27 +0600

    updated home

--- frontend/src/components/homeComponent/Home.js
+++ frontend/src/components/homeComponent/Home.js
@@ -1,6 +1,7 @@
 import { React, Fragment } from 'react';
 import './homepage.css';
 import '../../general.css';
+import './page.css';
 import img1 from './huhu.svg';
 import img2 from './user.svg';
 import image1 from './find_doctor.svg';
@@ -9,6 +10,12 @@ import image3 from './clinic-building.svg';
 import image4 from './location.svg';
 import image5 from './contact-us.svg';
 import image6 from './about-us.svg';
+import image7 from './awareness.svg';
+import c1 from './facebook.svg';
+import c2 from './instagram.svg';
+import c3 from './twitter.svg';
+import c4 from './whatsapp.svg';
+
 
 const Home = () => {
   const myProfile = () => {
@@ -31,25 +38,52 @@ const Home = () => {
         </button>
       </div>
     </header>
+    <div class="page-container">
+      <div class="page-content">
+        <div class="page-title">
+          Welcome to *name *
+        </div>
+        <div class="motto">
+          *motto likhbo*
+        </div>
+        <div class="page-description">
+          *decription*
+        </div>
+      </div>
+      <div>
+        *services*
+      </div>
+      <div class="services">
+
+        <button class="service-icons">
+          <img src={image1} alt="find doctor" className="find-doctor-icon" />
+          <div class="tool-tip">
+            Find Doctor
+          </div>
+        </button>
+        <button class="service-icons">
+          <img src={image2} alt="ambulance" className="ambulance-icon" />
+          <div class="tool-tip">
+            Emergency services
+          </div>
+        </button>
+        <button class="service-icons">
+          <img src={image3} alt="clinic" className="clinic-icon" />
+          <div class="tool-tip">
+            get an appointment
+          </div>
+        </button>
+        <button class="service-icons">
+          <img src={image7} alt="awareness" className="awareness-icon" />
+          <div class="tool-tip">
+            *ki naam dibo*
+          </div>
+        </button>
+      </div>
+    </div>
+
+
     <navbar class="navbar">
-      <div class="navbar-icons">
-        <img src={image1} alt="find doctor" className="find-doctor-icon" />
-        <div class="tool-tip">
-          Find Doctor
-        </div>
-      </div>
-      <div class="navbar-icons">
-        <img src={image2} alt="ambulance" className="ambulance-icon" />
-        <div class="tool-tip">
-          Emergency services
-        </div>
-      </div>
-      <div class="navbar-icons">
-        <img src={image3} alt="clinic" className="clinic-icon" />
-        <div class="tool-tip">
-          get an appointment
-        </div>
-      </div>
       <div class="navbar-icons">
         <img src={image4} alt="location" className="location-icon" />
         <div class="tool-tip">
@@ -72,7 +106,32 @@ const Home = () => {
           About Us
         </div>
       </div>
+      <div class="bottom-bar">
+        <div class="contact-icons">
+          <img src={c1} alt="contact us" />
+        </div>
+        <div class="contact-icons">
+          <img src={c2} alt="contact us" />
+          {/* <div class="tool-tip">
+          Contact Us
+        </div> */}
+        </div>
+        <div class="contact-icons">
+          <img src={c3} alt="contact us" />
+          {/* <div class="tool-tip">
+          Contact Us
+        </div> */}
+        </div>
+        <div class="contact-icons">
+          <img src={c4} alt="contact us" />
+          {/* <div class="tool-tip">
+          Contact Us
+        </div> */}
+        </div>
+      </div>
     </navbar>
+
+
 
   </Fragment>
 }
