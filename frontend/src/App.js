d75e9d592a429a16b2e977d22d9f0aeeb75c394f
Atika-Tabassum <tabassumsuchi6025@gmail.com>
2024-07-12 19:35:38 +0600

    demo homepage added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Profile from './components/profile';
 import Home from './components/homeComponent/Home';
+import Content from './components/contentComponent/Content';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/:userId/myprofile" element={<Profile />} />
           <Route path="/" element={<Home />} />
+          <Route path="/contents" element={<Content />} />
         </Routes>
       </Fragment>
     </Router>
---
+++ frontend/src/components/contentComponent/Content.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
+import "./Content.css";
+
+const Content = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [content, setContent] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`http://localhost:3001/contents`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (Array.isArray(data.data)) {
+          setContent(data.data);
+        } else {
+          console.error("Received data is not an array:", data);
+        }
+      })
+      .then(() => setIsLoading(false))
+      .catch((err) => {
+        console.log("Error received:", err);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <section>
+      <h1>Contents</h1>
+      <ul>
+        {Array.isArray(content) ? (
+          content.map((item) => (
+            <li key={item.content_id}>
+              <h2>{item.topic}</h2>
+              <p>{item.description}</p>
+              <ReactPlayer url={item.video} controls={true} width="100%" height="500px" />
+            </li>
+          ))
+        ) : (
+          <div>
+            <p>No content available</p>
+          </div>
+        )}
+      </ul>
+    </section>
+  );
+};
+
+export default Content;
