092a415c3fa01e310f891ac62f604226c7587d0b
Atika-Tabassum <109681326+Atika-Tabassum@users.noreply.github.com>
2024-07-13 12:58:47 +0600

    frontend added

--- frontend/src/components/notificationComponent/Notification.js
+++ frontend/src/components/notificationComponent/Notification.js
@@ -1,11 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
+import "../homeComponent/homepage.css";
+import "../homeComponent/page.css";
+import "../general/general.css";
+import img1 from "../general/logo.svg";
+import img2 from "../general/user.svg";
+import image3 from "../general/notification.svg";
+import image5 from "../homeComponent/contact-us.svg";
+import image6 from "../homeComponent/about-us.svg";
+import c1 from "../homeComponent/facebook.svg";
+import c2 from "../homeComponent/instagram.svg";
+import c3 from "../homeComponent/twitter.svg";
+import c4 from "../homeComponent/whatsapp.svg";
 
 const Notification = () => {
   const navigate = useNavigate();
   const userId = useParams().userId;
   const [isLoading, setIsLoading] = useState(true);
   const [notifications, setNotifications] = useState([]);
+  localStorage.setItem("userId", userId);
 
   const handleClick = async (campId, select) => {
     try {
@@ -22,6 +35,10 @@ const Notification = () => {
     } catch (error) {
       console.error("Error occurred while updating response:", error);
     }
+  };
+
+  const myProfile = () => {
+    navigate(`/${userId}/myprofile`);
   };
 
   useEffect(() => {
@@ -47,37 +64,99 @@ const Notification = () => {
   }
 
   return (
-    <section>
-      <h1>Notifications</h1>
-      <ul>
-        {Array.isArray(notifications) && notifications.length > 0 ? (
-          notifications.map((item) => (
-            <li key={item.notification_id}>
-              {item.description}
-              {item.type === "invitation" ? (
-                <div>
-                  <Link to={`/${item.camp_id}/camp_details`}>
-                    <p>Learn More About the campaign</p>
-                  </Link>
-                  <button onClick={() => handleClick(item.camp_id, 1)}>
-                    <p>Accept</p>
-                  </button>
-                  <button onClick={() => handleClick(item.camp_id, 0)}>
-                    <p>Deny</p>
-                  </button>
-                </div>
-              ) : (
-                <p></p>
-              )}
-            </li>
-          ))
-        ) : (
-          <div>
-            <p>No notifications available</p>
+    <Fragment>
+      <header>
+        <div className="header-icons">
+          <div className="logo-section">
+            <img src={img1} alt="logo" className="logo-icon" />
+            <div className="description">Personal Healthcare Assistant</div>
           </div>
-        )}
-      </ul>
-    </section>
+
+          <div className="spacer"></div>
+
+          <button className="user-dashboard" onClick={myProfile}>
+            <div className="user-section">
+              <img src={img2} alt="user" className="user-icon" />
+            </div>
+          </button>
+
+          <button className="user-dashboard">
+            <div className="user-section">
+              <img src={image3} alt="notifications" className="user-icon" />
+            </div>
+          </button>
+        </div>
+      </header>
+
+      <navbar className="navbar">
+        <div className="navbar-icons">
+          <img src={image5} alt="contact us" className="contact-us-icon" />
+          <div className="tool-tip">Contact Us</div>
+        </div>
+      </navbar>
+
+      <navbar className="navbar2">
+        <div className="bottom-icon">
+          <img src={image6} alt="about us" className="about-us-icon" />
+          <div className="tool-tip">About Us</div>
+        </div>
+      </navbar>
+
+      <div className="bottom-bar">
+        <div className="contact-icons">
+          <img src={c1} alt="contact us" />
+        </div>
+        <div className="contact-icons">
+          <img src={c2} alt="contact us" />
+        </div>
+        <div className="contact-icons">
+          <img src={c3} alt="contact us" />
+        </div>
+        <div className="contact-icons">
+          <img src={c4} alt="contact us" />
+        </div>
+      </div>
+
+      <div className="page-container">
+        <div className="page-content"></div>
+        <h1>Notifications</h1>
+        <ul>
+          {Array.isArray(notifications) && notifications.length > 0 ? (
+            notifications.map((item) => (
+              <li key={item.notification_id}>
+                {item.type === "invitation" ? (
+                  <div>
+                     {item.description}
+                    <Link to={`/${item.camp_id}/camp_details`}>
+                      <p>Learn More About the campaign</p>
+                    </Link>
+                    <button onClick={() => handleClick(item.camp_id, 1)}>
+                      <b>Accept</b>
+                    </button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button onClick={() => handleClick(item.camp_id, 0)}>
+                      <b>Deny</b>
+                    </button>
+                  </div>
+                ) : (
+                  <div>
+                    {item.description}
+                    <Link to={`/${item.camp_id}/camp_details`}> the camp
+                    </Link>
+                  </div>
+                )}
+                <br/>
+                <br/>
+              </li>
+            ))
+          ) : (
+            <div>
+              <p>No notifications available</p>
+            </div>
+          )}
+        </ul>
+      </div>
+    </Fragment>
   );
 };
 
